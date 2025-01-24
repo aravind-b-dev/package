8e91d7dd6f003babfc534a95971edb5291c23183
aravind <tmcs@123>
2025-01-24 16:24:46 +0530

    feat: add backgroundColor support in version 0.0.2

--- lib/src/my_custom_widget.dart
+++ lib/src/my_custom_widget.dart
@@ -3,16 +3,21 @@ import 'package:flutter/material.dart';
 class MyCustomWidget extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
+  final Color? backgroundColor;
 
   const MyCustomWidget({
     Key? key,
     required this.title,
     required this.onPressed,
+    this.backgroundColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
       onPressed: onPressed,
       child: Text(title),
     );
