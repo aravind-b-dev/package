486e1f5b48c8a8c90ce41b88346a482062a798b9
aravind <tmcs@123>
2025-01-24 16:25:21 +0530

    feat: add size customization in version 0.0.3

--- lib/src/my_custom_widget.dart
+++ lib/src/my_custom_widget.dart
@@ -3,23 +3,31 @@ import 'package:flutter/material.dart';
 class MyCustomWidget extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
-  final Color? backgroundColor;
+  final Color backgroundColor;
+  final double? width;
+  final double? height;
 
   const MyCustomWidget({
     Key? key,
     required this.title,
     required this.onPressed,
-    this.backgroundColor,
+    required this.backgroundColor,
+    this.width,
+    this.height,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+        ),
+        onPressed: onPressed,
+        child: Text(title),
       ),
-      onPressed: onPressed,
-      child: Text(title),
     );
   }
 }
